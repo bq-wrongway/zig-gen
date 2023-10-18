4d92d3175ab70d5dec10d94513e773cc28895803
bq-wrongway <sesa.zeljko@gmail.com>
2023-10-18 21:21:49 +0200

    added struct to define parameters, freeing memory at a single place, some cosmetic improvements done, and reduced overal variable use, and removed few of unused variables

--- src/main.zig
+++ src/main.zig
@@ -18,19 +18,27 @@ const allocator = gpa.allocator();
 
 const ArrayList = std.ArrayList;
 
-var list = ArrayList(u8).init(allocator);
-var pass_list = ArrayList(u8).init(allocator);
+var character_pool_list = ArrayList(u8).init(allocator);
+var password = ArrayList(u8).init(allocator);
+
+const Params = struct {
+    is_special: bool,
+    is_nummeric: bool,
+    is_uppercase: bool,
+    password_length: u32,
+};
+//possible toggles and parameters with their default values, user can ofcourse override this
+var p = Params{
+    .is_special = true,
+    .is_nummeric = true,
+    .is_uppercase = true,
+    .password_length = 15,
+};
 
 pub fn main() !void {
-    //possible toggles and parameters with their default values, user can ofcourse override this
-    var is_special: bool = true;
-    var is_nummeric: bool = true;
-    var is_uppercase: bool = true;
-    var password_length: u32 = 15;
     //adding lowercase letter right away since they are not optionals
-    try list.appendSlice(letters_lowercase);
+    try character_pool_list.appendSlice(letters_lowercase);
     //freeing memory of the password list, this should be freed either way at the program exit (at least what i think atm)
-    defer pass_list.deinit();
 
     const params = comptime clap.parseParamsComptime(
         \\-h, --help             Display this help and exit.
@@ -53,34 +61,41 @@ pub fn main() !void {
         .diagnostic = &diag,
     }) catch |err| {
         diag.report(stderr, err) catch {};
-        // print("{}", .{@TypeOf(err)});
         try stderr.print("You passed invalid character.Use --help to see more info.\n", .{});
         return;
     };
-    //freeing memory
-    defer res.deinit();
-    defer _ = gpa.deinit();
+
     //matching possible parameters
     if (res.args.help != 0)
         return clap.help(stderr, clap.Help, &params, .{});
     if (res.args.symbols != 0)
-        is_special = false;
+        p.is_special = false;
     if (res.args.length) |l|
-        password_length = @as(u32, @intCast(l));
+        p.password_length = @as(u32, @intCast(l));
     if (res.args.nummeric != 0)
-        is_nummeric = false;
+        p.is_nummeric = false;
     if (res.args.uppercase != 0)
-        is_uppercase = false;
+        p.is_uppercase = false;
     if (res.args.write != 0)
         print("I will save to file in future with custom name for the password field", .{});
 
-    const final_char_list = try passwordCharPool(is_uppercase, is_special, is_nummeric);
-    //i would like generate pass to return the u8 instead of void, but i get volatile asm error when attempting that
-    try generatePass(password_length, final_char_list);
+    const final_char_list = try passwordCharPool(p.is_uppercase, p.is_special, p.is_nummeric);
+    const mypas = try generatePass(p.password_length, final_char_list);
+
+    //freeing memory
+    defer {
+        password.deinit();
+        allocator.free(mypas);
+        allocator.free(final_char_list);
+        res.deinit();
+        _ = gpa.deinit();
+    }
+
+    try stdout_writer.print("{s}\n", .{mypas});
 }
 
 // randomizes password character
-fn generatePass(length: u32, char_list: []u8) !void {
+fn generatePass(length: u32, char_list: []u8) ![]u8 {
     var prng = std.rand.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.os.getrandom(std.mem.asBytes(&seed));
@@ -91,32 +106,26 @@ fn generatePass(length: u32, char_list: []u8) !void {
     var i: usize = 0;
     while (i < length) {
         var num = rand.intRangeAtMost(u8, 0, @as(u8, @intCast(char_list.len)) - 1);
-        try pass_list.append(char_list[num]);
+        try password.append(char_list[num]);
         i += 1;
     }
-    const final_pass = try pass_list.toOwnedSlice();
 
-    try stdout_writer.print("{!s}\n", .{final_pass});
-    defer allocator.free(char_list);
-    defer allocator.free(final_pass);
+    return password.toOwnedSlice();
 }
 
 // adds characters to the password based on user toggle
 fn passwordCharPool(uppercase: bool, special: bool, numeric: bool) ![]u8 {
-    defer list.deinit();
+    defer character_pool_list.deinit();
     if (special) {
-        try list.appendSlice(special_chars);
-        const new_pass = try pass_list.toOwnedSlice();
-
-        defer allocator.free(new_pass);
+        try character_pool_list.appendSlice(special_chars);
     }
     if (numeric) {
-        try list.appendSlice(numbers);
+        try character_pool_list.appendSlice(numbers);
     }
     if (uppercase) {
-        try list.appendSlice(letters_uppercase);
+        try character_pool_list.appendSlice(letters_uppercase);
     }
-    return try list.toOwnedSlice();
+    return try character_pool_list.toOwnedSlice();
 }
 
 fn saveToFile() void {
